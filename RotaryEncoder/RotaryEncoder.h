7b431bd39d9e4a8b41e13e51bcf7187c6ee38627
Andreas Watterott <aw@watterott.net>
2014-04-19 11:23:10 +0200

    added rotary encoder lib

---
+++ RotaryEncoder/RotaryEncoder.h
@@ -0,0 +1,29 @@
+#ifndef RotaryEncoder_h
+#define RotaryEncoder_h
+
+
+#include <inttypes.h>
+
+
+#define SW_PRESSED           (1)
+#define SW_PRESSEDLONG       (2)
+
+
+class RotaryEncoder
+{
+  public:
+    RotaryEncoder();
+
+    void init(void);
+    void service(void);
+    int_least8_t step(void);
+    int_least8_t sw(void);
+
+  private:
+    volatile int_least8_t re_sw;
+    volatile int_least8_t re_delta;
+    int_least8_t re_last;
+};
+
+
+#endif //RotaryEncoder_h
