58b8c4e615ca7b1bcbe59926be4fbb479e9f0813
Andreas Watterott <aw@watterott.net>
2015-01-16 20:17:55 +0100

    added RV8523 lib

---
+++ RV8523/RV8523.h
@@ -0,0 +1,26 @@
+#ifndef RV8523_h
+#define RV8523_h
+
+
+#include <inttypes.h>
+
+
+class RV8523
+{
+  public:
+    RV8523();
+
+    void start(void);
+    void stop(void);
+    void get(uint8_t *sec, uint8_t *min, uint8_t *hour, uint8_t *day, uint8_t *month, uint16_t *year);
+    void get(int *sec, int *min, int *hour, int *day, int *month, int *year);
+    void set(uint8_t sec, uint8_t min, uint8_t hour, uint8_t day, uint8_t month, uint16_t year);
+    void set(int sec, int min, int hour, int day, int month, int year);
+
+  private:
+    uint8_t bin2bcd(uint8_t val);
+    uint8_t bcd2bin(uint8_t val);
+};
+
+
+#endif //RV8523_h
